55c4fe1dccb11cc1ba9e893a7256348013bd9779
Olga <tunbergia@mail.ru>
2017-04-25 14:59:25 +0300

    scroll

--- src/js/script.js
+++ src/js/script.js
@@ -142,6 +142,14 @@ function openModal1(event) {
     }
   $('#link5').on('click', openModal5);
 
+
+  $("#nav").on("click","a", function (event) {
+event.preventDefault();
+var id = $(this).attr('href'),
+top = $(id).offset().top;
+$('body,html').animate({scrollTop: top}, 1500);
+});
+
   
 
 });
